999de37f163f57a7d9c3dd5d1b311434d7bc199b
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-26 16:39:55 +1300

    separate cap, so easy to reference in code and tests

---
+++ cap.js
@@ -0,0 +1,11 @@
+var chloride = require('chloride')
+
+function hash(s) {
+  return chloride.crypto_hash_sha256(
+    'string' == typeof s ? new Buffer(s, 'utf8') : s
+  )
+}
+
+module.exports =  hash("user-invites:development")
+
+
